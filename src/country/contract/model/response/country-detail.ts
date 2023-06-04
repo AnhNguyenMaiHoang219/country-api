86bb4be26dd79173daa95c22ee2d089e22384553
Anh Nguyen <nguyenhoanganh219@gmail.com>
2023-06-04 20:20:17 +0300

    Improve Swagger specs for countries endpoints

--- src/country/contract/model/response/country-detail.ts
+++ src/country/contract/model/response/country-detail.ts
@@ -1,19 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Exclude, Expose } from 'class-transformer';
 
 @Exclude()
 export class CountryDetailResponse {
+  @ApiProperty({ name: 'name', example: 'Finland' })
   @Expose({ name: 'name' })
   name: string;
 
+  @ApiProperty({ name: 'country_code', example: 'FI' })
   @Expose({ name: 'country_code' })
   countryCode: string;
 
+  @ApiProperty({ name: 'capital', example: 'Helsinki' })
   @Expose({ name: 'capital' })
   capital: string;
 
+  @ApiProperty({ name: 'population', example: '5530719' })
   @Expose({ name: 'population' })
   population: number;
 
+  @ApiProperty({ name: 'flag_file_url', example: 'https://flagcdn.com/w320/fi.png' })
   @Expose({ name: 'flag_file_url' })
   flagFileUrl: string;
 
--- src/country/contract/model/response/country.ts
+++ src/country/contract/model/response/country.ts
@@ -1,10 +1,13 @@
 import { Exclude, Expose } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Exclude()
 export class Country {
+  @ApiProperty({ name: 'name', example: 'Vietnam' })
   @Expose({ name: 'name' })
   name: string;
 
+  @ApiProperty({ name: 'country_code', example: 'VN' })
   @Expose({ name: 'country_code' })
   countryCode: string;
 
